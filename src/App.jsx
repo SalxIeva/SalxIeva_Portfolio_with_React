fe8febb3876f8661ce64bb1021c1194dd065163f
SalxIeva <ieva.saldukaite@gmail.com>
2024-01-07 17:27:27 +0000

    pseudo code added to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// imported components
 import Header from './Pages/Header';
 import About from './Pages/About';
 import Skills from './Pages/Skills';
@@ -7,11 +8,16 @@ import ProjectsPage from './Pages/ProjectsPage';
 import Project from './Pages/Project'
 
 function App() {
-    return (
-    <div>
-
-    </div>
-    )
+    return 
+  // set up router
+  // render header component
+  // switch between routes?
+  // home route (about componen)
+  // skills route
+  // projects route
+  // contact route 
+   
 }
 
+// export app
 export default App;
