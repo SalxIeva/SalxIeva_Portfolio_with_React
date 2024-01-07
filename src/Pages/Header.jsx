84ef2490cfe0c27a40d65bbde444f46007c2168d
SalxIeva <ieva.saldukaite@gmail.com>
2024-01-07 21:38:20 +0000

    Navbar added to Header.jsx

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -1,4 +1,15 @@
+import React from "react";
+import Navbar from './Components/Navbar';
+
 // should contain navBar
 // have to be able to navigate between different sections/pages of portfolio. (maybe add <Link> or <NavLink> and use react router)
 // make sure it is responsive and appears correctly on all pages
+function Header() {
+    return (
+        <header>
+            <Navbar />
+        </header>
+    )
+}
 // export header
+export default Header;
