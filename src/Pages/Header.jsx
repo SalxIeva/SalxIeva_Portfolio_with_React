c4168632ecbfca3d61387f64a75f6c97d766cabf
SalxIeva <ieva.saldukaite@gmail.com>
2024-01-08 07:44:51 +0000

    imports are done properly

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -1,14 +1,23 @@
 import React from "react";
-import Navbar from './Components/Navbar';
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import backgroundImage from '../assets/IMG_5650.jpg';
 
 // should contain navBar
 // have to be able to navigate between different sections/pages of portfolio. (maybe add <Link> or <NavLink> and use react router)
 // make sure it is responsive and appears correctly on all pages
 function Header() {
     return (
+        <div>
         <header>
             <Navbar />
+            <Hero backgroundImage={backgroundImage}>
+        <h1>Developer based in London</h1>
+        <h2>Details, creativity and passion brings all the right people together.</h2>
+      </Hero>
+      
         </header>
+        </div>
     )
 }
 // export header
