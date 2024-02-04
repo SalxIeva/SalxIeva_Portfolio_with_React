99f3f458f513a5eeb85323a557527f8fe839c19b
SalxIeva <ieva.saldukaite@gmail.com>
2024-02-04 19:00:38 +0000

    testing

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar/index";
 
 // should contain navBar
 // have to be able to navigate between different sections/pages of portfolio. (maybe add <Link> or <NavLink> and use react router)
