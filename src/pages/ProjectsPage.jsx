f359dbf93c816f0c40e6a6fa7b0a152b95a85983
SalxIeva <ieva.saldukaite@gmail.com>
2024-02-04 22:20:11 +0000

    folders added

--- src/pages/ProjectsPage.jsx
+++
@@ -1,4 +0,0 @@
-// create a container for project components
-// render project components based on the JSON data of my projects
-// ensure it is easy to navigate through them and well organized
-// export projectspage
