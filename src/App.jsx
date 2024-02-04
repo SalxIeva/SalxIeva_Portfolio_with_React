c77e898662b65fdaebaf3b8fbb7cac3f6d0c2390
SalxIeva <ieva.saldukaite@gmail.com>
2024-02-04 20:07:55 +0000

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // imported components
-import Header from './Pages/Header';
-import HomePage from './Pages/HomePage';
-import About from './Pages/About';
-import Skills from './Pages/Skills';
+import Header from './pages/Header';
+import HomePage from './pages/HomePage';
+import About from './pages/About';
+import Skills from './pages/Skills';
 // import ProjectsPage from './Pages/ProjectsPage';
 // import Project from './Pages/Project'
 
