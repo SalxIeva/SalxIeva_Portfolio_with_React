db09a12e3dd5cb8b0e3e63fe1304873eeb5c9650
SalxIeva <133906072+SalxIeva@users.noreply.github.com>
2024-03-15 18:01:50 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/SalxIeva_Portfolio_with_React/",
+  base: "/SalxIeva_Portfolio_with_React/",
   plugins: [react()],
 })
